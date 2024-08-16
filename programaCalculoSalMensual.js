2b134e171c4f7241a7e8d6037d1bd1f38e591bdb
Estefania9027 <108842526+Estefania9027@users.noreply.github.com>
2024-08-16 16:30:47 -0500

    Update programaCalculoSalMensual.js

--- programaCalculoSalMensual.js
+++ programaCalculoSalMensual.js
@@ -1,7 +1,7 @@
 let salarioMinimo2024 = 1300000
 let salarioMensual = 0;
-let aporteSalud = (salarioMensual * 12.5%);
-let aportePension = (salarioMensual * 16%);
+let aporteSalud = (salarioMensual * (12.5%));
+let aportePension = (salarioMensual * (16%));
 let cesantias = (salarioMensual / 12);
 let prima = (salarioMensual / 12);
 let auxiliotransp = (162000);
