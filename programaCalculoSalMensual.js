da00ad9c59aef9351aa3b6b3218cc4237cfb611a
Estefania9027 <108842526+Estefania9027@users.noreply.github.com>
2024-08-16 16:24:49 -0500

    Update programaCalculoSalMensual.js

--- programaCalculoSalMensual.js
+++ programaCalculoSalMensual.js
@@ -9,8 +9,8 @@ let auxiliotransp = (162000);
 salarioMensual = prompt("Me indica su salario mensual");
 
 if salarioMensual < salarioMinimo2024*2 {
-  let calculoIngresosMensual = alert("Su Ingreso mensual es" ${salarioMensual + aporteSalud + 
-                                                                     aportepension + cesantias + prima + auxiliotransp};
-else alert("Su Ingreso mensual es" ${salarioMensual + aporteSalud + aportepension + cesantias + prima};
+  let calculoIngresosMensual = alert("Su Ingreso mensual es: " ${salarioMensual + aporteSalud + 
+                                                                     aportepension + cesantias + prima + auxiliotransp});
+else alert("Su Ingreso mensual es: " ${salarioMensual + aporteSalud + aportepension + cesantias + prima});
   
 
